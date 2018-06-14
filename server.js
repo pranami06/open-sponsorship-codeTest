1f41e0185bd766e19e0844a4d2e863e276f7bc72
Pranami Jhawar <jhawar.pranami@gmail.com>
2018-06-14 14:17:52 -0400

    added server start code

--- server.js
+++ server.js
@@ -4,3 +4,14 @@ var fs = require('fs');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var Athlete = require('./model/athlete');
+
+// initializing the express instance
+var app = express();
+
+// initializing port number
+const port = 8000;
+
+// starting the server
+app.listen(port, () => {
+    console.log(`Server started on port : ${port}`);
+});
