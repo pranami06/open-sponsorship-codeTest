8b50662093ee331e35fcfacda62d9fad8961f499
Pranami Jhawar <jhawar.pranami@gmail.com>
2018-06-14 14:16:14 -0400

    Added initial Structure of the project

---
+++ model/athlete.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var athleteSchema = new mongoose.Schema({
+    name: String,
+    dob: String,
+    gender: String,
+    nationality: String,
+    selectedSport: Array,
+    location: String,
+    team: String,
+    married: String,
+    association: String,
+    about: String,
+    interests: String,
+    charities: String,
+    petsName: String,
+    drinksAlcohol: String,
+    facebook: String,
+    twitter: String,
+    instagram: String
+});
+
+var Athlete = mongoose.model('Athlete', athleteSchema);
+module.exports = Athlete;
---
+++ server.js
@@ -0,0 +1,6 @@
+// importing the modules required in the application
+var express = require('express');
+var fs = require('fs');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var Athlete = require('./model/athlete');
