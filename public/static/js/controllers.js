9763261c0228c60724d050ce1d47c58ecfa6e34e
Pranami Jhawar <jhawar.pranami@gmail.com>
2018-06-14 14:42:56 -0400

    added get method for fetching sports list

--- public/static/js/controllers.js
+++ public/static/js/controllers.js
@@ -1,2 +1,16 @@
 // angular module declaration
 var athleteControllers = angular.module('athleteControllers',[]);
+
+athleteModule.controller('athleteFormController', 
+    function($scope, $http){
+           
+        // requested sports Data from the server
+        $http.get('/sportsList').success(function(response){
+            $scope.sportsList = response;
+            console.log($scope.sportsList);
+        });
+        $scope.selectedSport =[];
+        $scope.athlete = {};
+        $scope.athlete =   {"name":"Pranami Jhawar","dob":"06/13/2018","gender":"Female","nationality":"United States","selectedSport":["Tennis","Basketball","American Football"],"location":"Radio City Music Hall","team":"New York Giants","isMarried":"No","association":"NBA","about":"I am awesome!","interests":"Reading, Singing","charities":"Americal Society","pets":"Kitty","ifDrinks":"Yes","facebook":"pranami@facebook.com","twitter":"@pranami","instagram":"@pranami"}        
+    }
+);
